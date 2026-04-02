e7ea6d2b72a82593cfb916891caabd0b6892d3e9
kdeary3 <kenodeary3@gmail.com>
2026-04-02 15:54:38 -0500

    linked list add, remove, sort, search

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,30 +1,18 @@
 public class LinkedList {
+
     public static void main(String[] args) {
 
-        Node node = new Node(10,null);
-        LinkedList list = new LinkedList(node);
-        list.addFirst(9);
-        list.addFirst(8);
-        list.addFirst(7);
-        list.addFirst(6);
-        list.addFirst(5);
-        list.addFirst(4);
-        list.addFirst(3);
-        list.addFirst(2);
-        list.addFirst(1);
+        LinkedList list = new LinkedList();
+        int[] array = {11, 4, 18, 7, 2, 15, 1, 9, 20, 13, 6, 17, 3, 10, 8, 19, 5, 14, 12, 16};
+        for (int i = 0; i < array.length; i++) {
+            list.add(array[i]);
+        }
 
-        list.addLast(11);
-        list.addLast(12);
-        list.addLast(13);
-        list.addLast(14);
-        list.addLast(15);
-        list.addLast(16);
-        list.addLast(17);
-        list.addLast(18);
-        list.addLast(19);
-        list.addLast(20);
-
+        list.removeFirst(); // removes 1
+        list.removeLast(); // removes 20
+        list.remove(14); // removes 14
         list.display();
+        System.out.println(list.search(8));
 
     }
 
@@ -69,6 +57,89 @@ public class LinkedList {
             ptr = ptr.getNext();
         }
         ptr.setNext(node);
+    }
+
+    public void removeFirst() {
+        if (head == null) {
+            return;
+        }
+        head = head.getNext();
+    }
+
+    public void removeLast() {
+        if (head == null) return;
+        if (head.getNext() == null) return;
+
+        Node ptr = head;
+        while (ptr.getNext().getNext() != null) {
+            ptr = ptr.getNext();
+        }
+        ptr.setNext(null) ;
+    }
+
+    public void remove(int data) {
+        if (head == null) return;
+
+        if (head.getData() == data) {
+            head = head.getNext();
+            return;
+        }
+
+        Node ptr = head;
+        while (ptr.getNext() != null) {
+            if (ptr.getNext().getData() == data) {
+                ptr.setNext(ptr.getNext().getNext());
+                return;
+            }
+            ptr = ptr.getNext();
+        }
+    }
+
+    public boolean search(int value) {
+        if (head == null) return false;
+
+        Node ptr = head;
+        while (ptr != null) {
+            if (ptr.getData() == value) {
+                return true;
+            }
+            ptr = ptr.getNext();
+        }
+        return false;
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+
+        // if head is empty or new value is less than current head
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node ptr = head;
+        Node prevPtr = null;
+
+        while ( (ptr.getData() < data) && (ptr.getNext() != null) ) {
+            prevPtr = ptr;
+            ptr = ptr.getNext();
+        }
+
+        if (ptr.getData() > data) {
+            if (prevPtr == null) {
+                head = newNode;
+                newNode.setNext(ptr);
+                return;
+            }
+            prevPtr.setNext(newNode);
+            newNode.setNext(ptr);
+            return;
+        }
+
+        if (ptr.getNext() == null) {
+            ptr.setNext(newNode);
+            return;
+        }
 
     }
 

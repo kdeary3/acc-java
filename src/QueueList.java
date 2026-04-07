c9f71e202c20c08e7e584dfe13432ab03faab7f0
kdeary3 <kenodeary3@gmail.com>
2026-04-07 15:48:30 -0500

    queue with list

--- src/QueueList.java
+++ src/QueueList.java
@@ -1,6 +1,28 @@
-public class Queue {
+import java.util.Queue;
+
+public class QueueList {
 
     public static void main(String[] args) {
+
+        QueueList queue = new QueueList();
+
+        Node head = new Node(1);
+        Node node = new Node(2);
+        Node rear = new Node(3);
+        Node node4 = new Node(4);
+
+        queue.enqueue(head);
+        queue.display();
+        queue.enqueue(node);
+        queue.display();
+        queue.enqueue(rear);
+        queue.display();
+        queue.dequeue();
+        queue.display();
+        queue.dequeue();
+        queue.display();
+        queue.enqueue(node4);
+        queue.display();
 
 
 
@@ -9,77 +31,77 @@ public class Queue {
     private Node front;
     private Node rear ;
 
-    public Queue(Node front, Node rear) {
+    public QueueList(Node front, Node rear) {
         this.front = front;
         this.rear = rear;
     }
 
-    public Queue() {
+    public QueueList() {
         front = null;
         rear = null;
     }
-
-    public Queue(Node front) {
+    public QueueList(Node front) {
         this.front = front;
-        rear = null;
     }
-
     public Node getFront() {
         return front;
     }
-
     public void setFront(Node front) {
         this.front = front;
     }
-
     public Node getRear() {
         return rear;
     }
-
     public void setRear(Node rear) {
         this.rear = rear;
     }
+
+    public boolean isEmpty() {
+        return front == null;
+    }
+
+    public boolean isFull() {
+        return front == rear;
+    }
+
+    public void enqueue(Node node) {
+        if (isEmpty()) {
+            front = node;
+            rear = node;
+        } else {
+            rear.setNext(node);
+            rear = node;
+        }
+    }
+
+    public Node dequeue() {
+        if (isEmpty()){
+            return null;
+        }
+        else {
+            Node temp = front;
+            front = front.getNext();
+            return temp;
+        }
+    }
+
+    public int peek() {
+        if (front == null){
+            return -1;
+        }
+        else {
+            return front.getData();
+        }
+    }
+
+    public void display() {
+        if (front == null) return;
+
+        Node ptr = front;
+        while (ptr != null) {
+            System.out.print(ptr.getData() + " ");
+            ptr = ptr.getNext();
+        }
+        System.out.println("");
+    }
 }
-
-class Node {
-    private int data ;
-    private Node next ;
-
-    public Node() {
-        this.data = 0;
-        this.next = null;
-    }
-
-    public Node(int data, Node next) {
-        this.data = data;
-        this.next = next;
-    }
-
-    public Node(int data) {
-        this.data = data;
-        this.next = null;
-    }
-
-    public int getData() {
-        return data;
-    }
-
-    public void setData(int data) {
-        this.data = data;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-
-    @Override
-    public String toString() {
-        return "Node{" +
-                "data=" + data +
-                ", next=" + next +
-                '}';
-    }

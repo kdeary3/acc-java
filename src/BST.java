6fc8e8e015c8cd442d67698e6ec415877d9858eb
kdeary3 <kenodeary3@gmail.com>
2026-04-14 15:49:08 -0500

    BST

--- src/BST.java
+++ src/BST.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class BST {
 
     public static void main(String[] args) {
@@ -11,12 +13,25 @@ public class BST {
         bstRec.addNodeRecursive(5);
         bstRec.addNodeRecursive(11);
         bstRec.addNodeRecursive(7);
+        bstRec.addNodeRecursive(6);
+        bstRec.addNodeRecursive(4);
+        bstRec.addNodeRecursive(12);
+        bstRec.addNodeRecursive(54);
+        bstRec.addNodeRecursive(56);
+        bstRec.addNodeRecursive(77);
+        bstRec.addNodeRecursive(34);
+        bstRec.addNodeRecursive(1234);
+        bstRec.addNodeRecursive(23);
+        bstRec.addNodeRecursive(234);
+        bstRec.addNodeRecursive(22);
+        bstRec.addNodeRecursive(345);
+        bstRec.addNodeRecursive(4434);
 
-        System.out.println(bst);
-        System.out.println(bstRec);
+        bstRec.inOrderPrint(bstRec.getRoot());
+//        System.out.println(bstRec.search(1234));
+        System.out.println(bstRec.searchRecursive(bstRec.getRoot(), 1234));
 
     }
-
 
     private TreeNode root;
 
@@ -45,14 +60,13 @@ public class BST {
         }
 
         TreeNode ptr = root;
-        TreeNode prev = null ;
+        TreeNode prev = null;
 
         while (ptr != null) {
             prev = ptr;
             if (data > ptr.getData()) {
                 ptr = ptr.getRight();
-            }
-            else {
+            } else {
                 ptr = ptr.getRight();
             }
         }
@@ -81,6 +95,120 @@ public class BST {
         }
 
         return node;
+    }
+
+    // recursive prints
+    public void inOrderPrint(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        TreeNode ptr = node;
+
+        if (ptr != null) {
+            inOrderPrint(ptr.left);
+            System.out.print(ptr.getData() + " ");
+            inOrderPrint(ptr.right);
+        }
+    }
+
+    public void preOrderPrint(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        TreeNode ptr = node;
+
+        if (ptr != null) {
+            System.out.print(ptr.getData() + " ");
+            inOrderPrint(ptr.left);
+            inOrderPrint(ptr.right);
+        }
+    }
+
+    public void postOrderPrint(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        TreeNode ptr = node;
+
+        if (ptr != null) {
+            inOrderPrint(ptr.left);
+            inOrderPrint(ptr.right);
+            System.out.print(ptr.getData() + " ");
+        }
+    }
+
+    public boolean search(int data) {
+        if (root == null) {
+            return false;
+        }
+
+        TreeNode ptr = root;
+
+        while (ptr != null) {
+            if (data == ptr.getData()) {
+                return true;
+            }
+
+            if (data > ptr.getData()) {
+                ptr = ptr.getRight();
+            } else {
+                ptr = ptr.getLeft();
+            }
+        }
+        return false;
+    }
+
+    public boolean searchRecursive(TreeNode node, int data) {
+        if (node == null) return false;
+        if (data == node.getData()) return true;
+
+        if (data > node.getData()) {
+            return searchRecursive(node.getRight(), data);
+        } else {
+            return searchRecursive(node.getLeft(), data);
+        }
+    }
+
+    public void delete(int data) {
+        if (root == null) return;
+        if (search(data) == false) {
+            System.out.println("Node not found");
+            return;
+        }
+
+        TreeNode ptr = root;
+        TreeNode prev = null;
+
+        while (ptr != null) {
+            if (data == ptr.getData()) break;
+
+            if (data > ptr.getData()) {
+                prev = ptr;
+                ptr = ptr.getRight();
+            } else {
+                prev = ptr;
+                ptr = ptr.getLeft();
+            }
+        }
+
+        if (ptr.getData() == root) {
+
+        }
+
+        if (ptr.getLeft() == null && ptr.getRight() == null) {
+            if (prev.getLeft().getData() == data) {
+                ptr.setLeft(null);
+                return;
+            } else {
+                ptr.setRight(null);
+                return;
+            }
+        }
+        if ((ptr.getRight() != null && ptr.getLeft() != null) || (ptr.getLeft() == null && ptr.getRight() != null)) {
+            prev.setLeft(ptr.getRight());
+        }
     }
 
     @Override
